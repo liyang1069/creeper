16ffc32d13ac1a98f5e76d018ff0fd2742838fe3
liyang <18678395180@163.com>
2017-02-13 16:30:09 +0800

    first commit

---
+++ src/main/java/com/creeper/CreeperApplication.java
@@ -0,0 +1,12 @@
+package com.creeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreeperApplication.class, args);
+	}
+}
---
+++ src/test/java/com/creeper/CreeperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.creeper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CreeperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
